48ff8e1fb61a766b689da4cfac2391bf34db2745
david <jgarcia.ext@aenor.com>
2020-01-15 16:48:08 +0100

    example kafka with java, consumer and producer

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,38 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "test-group");
+
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
+        List topics = new ArrayList();
+        topics.add("kafka-test");
+        kafkaConsumer.subscribe(topics);
+        try{
+            while (true){
+                ConsumerRecords records = kafkaConsumer.poll(10);
+                for (Object record: records){
+                    System.out.println(String.format("Topic - %s, Partition - %d, Value: %s",
+                            ((ConsumerRecord)record).topic(),
+                            ((ConsumerRecord)record).partition(),
+                            ((ConsumerRecord)record).value()));
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            kafkaConsumer.close();
+        }
+    }
+}
